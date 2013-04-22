2f10c0e2712b35b09e1180c3287b650494ee1599
Kannan Manickam <kannan@rightscale.com>
2013-04-22 12:50:02 -0700

    Generated metadata.json from metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,3 @@ license          "Apache 2.0"
 description      "Installs/Configures Rackspace Cloud Monitoring"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.5"
-
-
-
